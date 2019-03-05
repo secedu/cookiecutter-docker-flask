ead5d08384d6e5ae0b425c73eff113a69ec22fa4
Carey Li <hello@carey.li>
2019-03-05 14:35:48 +1100

    Parameterise schema

--- app/schema.sql
+++ app/schema.sql
@@ -1,19 +1,7 @@
-DROP DATABASE IF EXISTS portal;
-CREATE DATABASE portal;
+DROP DATABASE IF EXISTS {{cookiecutter.database_name}};
+CREATE DATABASE {{cookiecutter.database_name}};
 
-\c portal
+\c {{cookiecutter.database_name}}
 
-DROP TABLE IF EXISTS admins;
-
-CREATE TABLE IF NOT EXISTS admins (
-    id INT PRIMARY KEY,
-    email TEXT NOT NULL,
-    password TEXT,
-    session TEXT
-);
-
-INSERT INTO admins VALUES 
-    (0, 'admin@ns.agency', 'bernie_sanders_4_president', '76f46c14b962e97feeea194630efa1196f474e7bd53daa873be5a3fb3df9e2f0');
-
-CREATE USER break1 WITH ENCRYPTED PASSWORD 'break1';
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO break1;
+CREATE USER {{cookiecutter.database_user}} WITH ENCRYPTED PASSWORD '{{cookiecutter.database_pass}}';
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO {{cookiecutter.database_user}};
