378ea06498a094833d09973bd9424136c61d713e
Carey Li <hello@carey.li>
2019-03-06 15:53:58 +1100

    Fixup run.py

--- {{cookiecutter.project_slug}}/run.py
+++ {{cookiecutter.project_slug}}/run.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 
 try:
-    from flaskr import create_app
+    import {{cookiecutter.project_slug}}
 except ImportError:
     raise RuntimeError("Couldn't import flaskr, have you run pip install -e app?")
 
-app = create_app()
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=4141, debug=True, use_evalex=False)
+    {{cookiecutter.project_slug}}.app.run(host="0.0.0.0", port=4141, debug=True, use_evalex=False)
