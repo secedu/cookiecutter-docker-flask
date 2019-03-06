488d45a5e82f3e3658df7046889cc62e8a432118
Carey Li <hello@carey.li>
2019-03-06 15:45:07 +1100

    Revert "Make run.py compatible"
    This reverts commit 20d6c6d85e491f2b729fcec0d63a16963ee0328c.

--- {{cookiecutter.project_slug}}/run.py
+++ {{cookiecutter.project_slug}}/run.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 try:
-    from {{cookiecutter.project_slug}} import app
+    from flaskr import create_app
 except ImportError:
-    raise RuntimeError("Couldn't import {{cookiecutter.project_slug}}, have you run pip install -e app?")
+    raise RuntimeError("Couldn't import flaskr, have you run pip install -e app?")
+
+app = create_app()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=4141, debug=True, use_evalex=False)
