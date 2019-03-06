20d6c6d85e491f2b729fcec0d63a16963ee0328c
Carey Li <hello@carey.li>
2019-03-06 15:27:50 +1100

    Make run.py compatible

--- {{cookiecutter.project_slug}}/run.py
+++ {{cookiecutter.project_slug}}/run.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 
 try:
-    from flaskr import create_app
+    from {{cookiecutter.project_slug}} import app
 except ImportError:
-    raise RuntimeError("Couldn't import flaskr, have you run pip install -e app?")
-
-app = create_app()
+    raise RuntimeError("Couldn't import {{cookiecutter.project_slug}}, have you run pip install -e app?")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=4141, debug=True, use_evalex=False)
