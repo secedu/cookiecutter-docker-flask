c8dd72c8663ac8dac24e8932883103dd7a0f9387
Carey Li <hello@carey.li>
2019-03-06 23:02:16 +1100

    Add loading from config.py

--- {{cookiecutter.project_slug}}/run.py
+++ {{cookiecutter.project_slug}}/run.py
@@ -3,7 +3,17 @@
 try:
     import {{cookiecutter.project_slug}}
 except ImportError:
-    raise RuntimeError("Couldn't import flaskr, have you run pip install -e app?")
+    raise RuntimeError("Couldn't import {{cookiecutter.project_slug}}, have you run pip install -e app?")
+
+import os
 
 if __name__ == "__main__":
+    config_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        'config.py'
+    )
+
+    print("Loading config from", config_path)
+    {{cookiecutter.project_slug}}.app.config.from_pyfile(config_path, silent=True)
+
     {{cookiecutter.project_slug}}.app.run(host="0.0.0.0", port=4141, debug=True, use_evalex=False)
